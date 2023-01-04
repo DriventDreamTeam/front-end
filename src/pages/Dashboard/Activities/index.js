9d651bb871fcf6ed960a0ca3739da4c2ab2d45b1
joaofelix <joaofelix27@gmail.com>
2023-01-04 19:12:50 -0300

    feat: activity page when there isn't a payment

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,25 @@
+import { useContext, useState, useEffect } from 'react';
+import UserContext from '../../../contexts/UserContext';
+import useTicket from '../../../hooks/api/useTicket';
+import { getPayment } from '../../../services/paymentsApi';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { userData } = useContext(UserContext);
+  const [payment, setPayment] = useState({});
+  const { ticket } = useTicket();
+
+  useEffect(() => {
+    if (ticket) {
+      getPayment(userData?.token, ticket?.id).then((res) => {
+        setPayment({ ...res });
+      }).catch(error => {
+      });
+    }
+  }, [ticket]);
+
+  return  (
+    <>
+      {Object.keys(payment).length!==0 ? 'Atividades: Em breve!' : 'Efetue o pagamento antes'}
+    </>
+  );
 }
